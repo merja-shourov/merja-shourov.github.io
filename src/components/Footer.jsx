1d861300d308b6df99822476fdd52aafb0aab5cb
merja-shourov <merjashourov13@gmail.com>
2024-10-19 19:23:33 +0600

    update

---
+++ src/components/Footer.jsx
@@ -0,0 +1,6 @@
+function Footer(){
+    return <footer >
+        <span className="text-3xl bg-slate-400"> merjaShourov </span>
+    </footer>
+}
+export default Footer;
---
+++ src/components/Navber.jsx
@@ -0,0 +1,9 @@
+function Navber(){
+    return <nav>
+        <a href="#">Home</a>
+        <a href="#">Home</a>
+        <a href="#">Home</a>
+        <a href="#">Home</a>
+    </nav>
+}
+export default Navber;
