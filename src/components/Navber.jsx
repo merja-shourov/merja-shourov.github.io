a7bc8f7f4c7e4c7e4a4ce2c6658139ac9e17571b
merja-shourov <merjashourov13@gmail.com>
2024-12-27 16:11:39 +0600

    remove

--- src/components/Navber.jsx
+++
@@ -1,9 +0,0 @@
-function Navber(){
-    return <nav>
-        <a href="#">Home</a>
-        <a href="#">Home</a>
-        <a href="#">Home</a>
-        <a href="#">Home</a>
-    </nav>
-}
-export default Navber;
